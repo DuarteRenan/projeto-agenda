f9efc43938fa95d310374f550280df074f836952
Renan Duarte <duarterenan@hotmail.com>
2026-03-13 00:54:29 -0300

    Corrigindo a porta do servidor para rodar no Railway

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const routes = require('./routes');
 const path = require('path');
 const helmet = require('helmet');
 const csrf = require('csurf');
+const port = process.env.PORT || 3000;
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
 app.use(helmet());
@@ -50,9 +51,12 @@ app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
 
+app.set('trust proxy', 1);
+
 app.on('pronto', () => {
-  app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
+  app.listen(port, () => {
+    console.log(`Servidor executando`);
   });
 });
+
+
